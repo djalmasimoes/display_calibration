2edc8e49fc37bd454fd010b6b94f961873a73b37
Djalma <djalma.simoes1@gmail.com>
2019-06-09 14:08:24 +0200

    Organize forlders

--- codility.py
+++
@@ -1,9 +0,0 @@
-def solution(A, B):
-    # The fution returns the number of bits
-    binary = bin(A * B)
-    setBits = [ones for ones in binary[2:] if ones == '1']
-    return len(setBits)
-
-f = solution (3, 7)
-print(f)
-
