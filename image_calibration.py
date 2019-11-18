37ad9b9e473fa60023eb1e364aff6783ba853297
Djalma <djalma.simoes1@gmail.com>
2019-11-18 16:40:42 +0100

    Organise folders

---
+++ image_calibration.py
@@ -0,0 +1,29 @@
+import numpy as np
+from tensorflow import keras
+from PIL import Image
+import imageio
+
+# Load the models, including weights and optimizer.
+model_1 = keras.models.load_model('ddl_to_jnd.h5')
+model_2 = keras.models.load_model('jnd_correction.h5')
+model_3 = keras.models.load_model('jnd_to_ddl.h5')
+
+# Name and path of the image
+file = 'TG270-pQC.tif'
+im = Image.open('./Test patterns TG270/'+file)
+
+# Run the models
+pixel = np.array(im).ravel()
+jnd_predicted = model_1.predict(pixel).flatten()  # pixel to luminance
+jnd_corrected = model_2.predict(jnd_predicted).flatten()  # jnd correction
+pixel_corrected = model_3.predict(jnd_corrected).flatten()  # jnd to pixel
+pixel_corrected = pixel_corrected.astype(int)
+
+# Correct the image
+for i in range(len(pixel)):
+    pixel[i] = pixel_corrected[i]
+
+# Save the image
+image_calibrated = np.reshape(pixel, (np.array(im).shape[0], np.array(im).shape[1]))
+imageio.imwrite('cor_'+file, image_calibrated)
+print('cor_'+file+' saved...')
